375c5e194a42cd5811c6703b6ed3473388ec195a
mvricole <marian.borja@gmail.com>
2022-12-18 15:29:22 -0800

    Attempt 2 to get env to work.

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "@08Twelve",
-  database: "employees",
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+  
 });
 
 connection.connect(function (err) {
